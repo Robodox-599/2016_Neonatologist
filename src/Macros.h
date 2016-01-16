991d0780d8f14598492710d5dec7b1af52c6b484
Amy Contreras <amycontreras9@yahoo.com>
2016-01-16 11:21:32 -0800

    Added two new buttons for the Shooter manipulator, buttons A and B will get their actual button numbers assigned later. A shoots up, B reverts back down.

--- src/Macros.h
+++ src/Macros.h
@@ -1,3 +1,9 @@
 #include "WPILib.h"
 
+//TODO: Get real values. 
 #define SHOOTER_MOTOR_CHANNEL			(1)
+#define SHOOTER_BUTTON_A				(1)
+#define SHOOTER_BUTTON_B				(1)		
+
+//oi
+#define JOYSTICK_PORT					(2)
