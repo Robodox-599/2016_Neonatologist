5a34e6845a7ae451495e48d662f22f1e703ef2dc
Amy Contreras <amycontreras9@yahoo.com>
2016-01-18 09:52:47 -0800

    Re added two buttons for the shooter. One to shoot, one to reset the shooter.

--- src/Macros.h
+++ src/Macros.h
@@ -2,7 +2,8 @@
 
 //TODO: Get real values. 
 #define SHOOTER_MOTOR_CHANNEL			(1)
-#define CATAPULT_RESET_BUTTON			(1)
+#define SHOOTER_BUTTON					(1)
+#define SHOOTER_RESET_BUTTON			(1)
 
 //oi
 #define JOYSTICK_PORT					(2)
