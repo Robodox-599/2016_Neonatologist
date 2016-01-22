8e61b4098da3ed92fcf7ef75c40d01d0f880449e
Jas <41971x@gmail.com>
2016-01-21 17:29:53 -0800

    added chanels for the intake pivot encoder

--- src/Macros.h
+++ src/Macros.h
@@ -12,5 +12,8 @@
 #define RIGHT_INTAKE_MOTOR_CHANNEL			(1)
 #define PIVOT_MOTOR_CHANNEL					(1)
 
+#define PIVOT_ENCODER_CHANNEL_A				(2)
+#define PIVOT_ENCODER_CHANNEL_B				(2)
+
 #define INTAKE_SPEED						(1.0)
 #define PIVOT_SPEED							(2)
