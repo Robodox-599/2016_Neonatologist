d1700b83bf57037a138e04f3e54daa2c2ffb1e73
Jas <41971x@gmail.com>
2016-01-23 09:10:46 -0800

    added shooter encoder channels in macros

--- src/Macros.h
+++ src/Macros.h
@@ -5,5 +5,8 @@
 #define SHOOTER_BUTTON					(1)
 #define SHOOTER_RESET_BUTTON			(1)
 
+#define SHOOTER_ENCODER_CHANNEL_A		(1)
+#define SHOOTER_ENCODER_CHANNEL_B		(1)
+
 //oi
 #define JOYSTICK_PORT					(2)
