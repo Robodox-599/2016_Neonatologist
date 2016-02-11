bfce2c57d1ed93fdf3d3bbcac09d55466e73349f
amycontreras <amycontreras9@yahoo.com>
2016-02-11 15:31:07 -0800

    Updated values for joystick, motor, and double solenoid ports.

--- src/Macros.h
+++ src/Macros.h
@@ -1,15 +1,19 @@
 #include "WPILib.h"
 
 //TODO: Get real values. 
-#define SHOOTER_MOTOR_CHANNEL			(1)
+#define SHOOTER_MOTOR_CHANNEL			(10)
 #define SHOOTER_BUTTON					(1)
 #define SHOOTER_RESET_BUTTON			(1)
+//ok
+#define PISTON_BUTTON					(2)
+#define REVERSE_PISTON					(1)
 
+//get val 
 #define SHOOTER_ENCODER_CHANNEL_A		(1)
 #define SHOOTER_ENCODER_CHANNEL_B		(1)
 
-#define GEAR_PISTON_CHANNEL_A			(1)
+#define GEAR_PISTON_CHANNEL_A			(0)
 #define GEAR_PISTON_CHANNEL_B			(1)
 
 //oi
-#define JOYSTICK_PORT					(2)
+#define JOYSTICK_PORT					(0)
