d76c254dfe90ced7f2b587839485573b0ca71a4c
Amy <amycontreras9@yahoo.com>
2016-02-13 14:35:50 -0800

    Got correct port numbers for basic drive.

--- src/Macros.h
+++ src/Macros.h
@@ -7,12 +7,14 @@
 // drive variables
 #define DEADZONE 	(.15)
 
-// TODO: get motor ports
-#define FRONT_LEFT_MOTOR_PORT			(1)
+// TODO: fix later if the front/left things are off? otherwise should be good
+#define FRONT_LEFT_MOTOR_PORT			(3)
 #define FRONT_RIGHT_MOTOR_PORT			(1)
-#define BACK_LEFT_MOTOR_PORT			(1)
-#define BACK_RIGHT_MOTOR_PORT			(1)
+#define BACK_LEFT_MOTOR_PORT			(4)
+#define BACK_RIGHT_MOTOR_PORT			(2)
 
 // TODO: get shifter ports hopefully please salgado and corine liSTEN TO ME
+/*
 #define SHIFTER_PORT_A					(0)
 #define SHIFTER_PORT_B					(0)
+*/
