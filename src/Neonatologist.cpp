ae478d6d8bfa7bb28b9ab1f64792769bf33dd8a0
Jas <41971x@gmail.com>
2016-02-13 10:59:14 -0800

    fixed INCLUDES and removed auto stuff gottdamn

--- src/Neonatologist.cpp
+++ src/Neonatologist.cpp
@@ -1,37 +1,22 @@
 #include "Macros.h"
-#include "Drive\Drive.h"
-#include "Intake\Intake.h"
-#include "Lift\Lift.h"
-#include "Shooter\Shooter.h"
+#include "Drive/Drive.h"
+#include "Intake/Intake.h"
+#include "Lift/Lift.h"
+#include "Shooter/Shooter.h"
 
 class Neonatologist: public IterativeRobot
 {
 private:
 	LiveWindow *lw = LiveWindow::GetInstance();
-	SendableChooser *chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
-	std::string autoSelected;
 
 	void RobotInit()
 	{
-		chooser = new SendableChooser();
-		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
-		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
-		SmartDashboard::PutData("Auto Modes", chooser);
+
 	}
 
 	void AutonomousInit()
 	{
-		autoSelected = *((std::string*)chooser->GetSelected());
-		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
-
-		if(autoSelected == autoNameCustom){
-			//Custom Auto goes here
-		} else {
-			//Default Auto goes here
-		}
+		
 	}
 
 	void AutonomousPeriodic()
@@ -55,4 +40,4 @@ private:
 	}
 };
 
-START_ROBOT_CLASS(Neonatologist)
+START_ROBOT_CLASS(Neonatologist);
