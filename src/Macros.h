7726a1ad5b7ccec562b62b827abc767fb2139821
Jas <41971x@gmail.com>
2016-02-13 09:04:37 -0800

    'CHANNEL'->'MOTORS' and spacing fixes

--- src/Macros.h
+++ src/Macros.h
@@ -3,8 +3,8 @@
 // drive variables
 #define DEADZONE 	(.15)
 
-// TODO: get correct port numbers for the motor channels from the electronics people
-#define FRONT_LEFT_MOTOR_CHANNEL			(1)
-#define FRONT_RIGHT_MOTOR_CHANNEL			(1)
-#define BACK_LEFT_MOTOR_CHANNEL				(1)
-#define BACK_RIGHT_MOTOR_CHANNEL			(1)
+// TODO: get motor ports
+#define FRONT_LEFT_MOTOR_PORT			(1)
+#define FRONT_RIGHT_MOTOR_PORT			(1)
+#define BACK_LEFT_MOTOR_PORT			(1)
+#define BACK_RIGHT_MOTOR_PORT			(1)
