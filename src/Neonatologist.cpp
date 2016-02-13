d66e5bc401a9600c6e6e9e9a6f449b177c0da578
Jas <41971x@gmail.com>
2016-02-13 10:50:02 -0800

    I HATE AUTO COMMENTS

--- src/Neonatologist.cpp
+++ src/Neonatologist.cpp
@@ -21,16 +21,6 @@ private:
 		SmartDashboard::PutData("Auto Modes", chooser);
 	}
 
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	void AutonomousInit()
 	{
 		autoSelected = *((std::string*)chooser->GetSelected());
@@ -46,11 +36,7 @@ private:
 
 	void AutonomousPeriodic()
 	{
-		if(autoSelected == autoNameCustom){
-			//Custom Auto goes here
-		} else {
-			//Default Auto goes here
-		}
+
 	}
 
 	void TeleopInit()
