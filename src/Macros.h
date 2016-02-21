c2d73377cef96de7a1b1bc48b816fb0212edf7d7
Jas <41971x@gmail.com>
2016-02-20 17:27:36 -0800

    tidying up macros file for readability

--- src/Macros.h
+++ src/Macros.h
@@ -1,53 +1,37 @@
 #include "WPILib.h"
 
-// PORTS ARE BUENISIMO
-#define SHOOTER_MOTOR_CHANNEL			(5)
-#define SHOOTER_BUTTON					(2)
-#define SHOOTER_RESET_BUTTON			(1)
-//ok
-#define PISTON_BUTTON					(2)
-#define REVERSE_PISTON					(1)
-
-//get val 
-#define SHOOTER_ENCODER_CHANNEL_A		(1)
-#define SHOOTER_ENCODER_CHANNEL_B		(1)
-
-#define GEAR_PISTON_CHANNEL_A			(0)
-#define GEAR_PISTON_CHANNEL_B			(1)
-
-//oi
+// ~~~~~ joystick
 #define JOYSTICK_PORT					(0)
-
-// joystick values
 #define X_AXIS_R						(4)
 #define Y_AXIS_R						(5)
-#define INTAKE_BUTTON					(1)		// a
-#define OUTTAKE_BUTTON					(3)		// x
+#define INTAKE_BUTTON					(1)		// A
+#define OUTTAKE_BUTTON					(3)		// X
 #define INTAKE_PIVOT					(1)
 #define TOGGLE_GEARS					(2)		// B
+#define LOCK_PIVOT_BUTTON				(0)		// not implemented yet
 
-// drive variables
-#define DEADZONE 	(.25)
-
-// PORTS ARE GOOD
+// ~~~~~ Drive
+#define DEADZONE						(.25)
+#define GEAR_PISTON_CHANNEL_A			(0)
+#define GEAR_PISTON_CHANNEL_B			(1)
 #define FRONT_LEFT_MOTOR_PORT			(3)
 #define FRONT_RIGHT_MOTOR_PORT			(1)
 #define BACK_LEFT_MOTOR_PORT			(4)
 #define BACK_RIGHT_MOTOR_PORT			(2)
-
-// TODO: get shifter ports hopefully please salgado and corine liSTEN TO ME
 #define SHIFTER_PORT_A					(2)
 #define SHIFTER_PORT_B					(3)
 
-// PORTS ARE GOOD
-#define INTAKE_ROLLER_MOTOR_CHANNEL			(8)
-#define PIVOT_MOTOR_CHANNEL					(0)
+// ~~~~~ Intake
+#define INTAKE_ROLLER_MOTOR_CHANNEL		(8)
+#define PIVOT_MOTOR_CHANNEL				(0)
+#define INTAKE_SPEED					(1.0)
+#define PIVOT_SPEED						(2)
+#define PIVOT_ENCODER_CHANNEL_A			(2)
+#define PIVOT_ENCODER_CHANNEL_B			(2)
 
-// TODO: get correct values
-#define PIVOT_ENCODER_CHANNEL_A				(2)
-#define PIVOT_ENCODER_CHANNEL_B				(2)
-
-#define INTAKE_SPEED						(1.0)
-#define PIVOT_SPEED							(2)
-
-#define LOCK_PIVOT_BUTTON					(0)
+// ~~~~~ Shooter
+#define SHOOTER_MOTOR_CHANNEL			(5)
+#define SHOOTER_BUTTON					(2)
+#define SHOOTER_RESET_BUTTON			(1)
+#define SHOOTER_ENCODER_CHANNEL_A		(1)
+#define SHOOTER_ENCODER_CHANNEL_B		(1)
